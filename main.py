ddb16d2ed602098f1e19190a43676b1cb2de7072
Bailey Henry <71393570+Bailsss@users.noreply.github.com>
2020-09-16 11:18:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 print('hi')
 input('how are you ')
 print('oh well')
-input('can you ask me how i am now ')
+hru = input('can you ask me how i am now ')
+print("Thank you. I'm good, thanks for finally asking")
+
+if hru.lower = "no":
+  input("Please?")
+
